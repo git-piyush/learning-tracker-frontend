e82774e2c5a3b74dba1118bbf9707a6889409d64
Piyush <kmrpiyush95@gmail.com>
2026-03-19 09:49:04 +0530

    bug fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -423,6 +423,14 @@ loadSubscribedMatches(){
     if (card.title === 'New Feedback') {
         this.router.navigate(['/feedback-list']);
     }
+
+    if (card.title === 'Your Bookmarked') {
+        this.router.navigate(['/bookmarked-question']);
+    }
+
+    if (card.title === 'Questions Added by You') {
+        this.router.navigate(['/all-question']);
+    }
   }
 
   selected: boolean[] = [];
